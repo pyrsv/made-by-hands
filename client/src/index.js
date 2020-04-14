843b67dd441bf7837e27ed8e1a89a167c8039399
Nikita Pyresev <wegofender@gmail.com>
2020-04-14 20:03:02 +0300

    add ThemeProvider

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import App from './App';
+
+const theme = {
+	lightPink: '#F9ECEF', // main light background color
+	mainDark: '#16161C', // color for text, buttons and dark borders
+	mainOlive: '#BBC8A9', // main olive background
+	whiteBackground: '#FFFCFC', // background for modals and dropdowns
+	lightBorder: '#747A82', // ligth border color, used in modal forms
+};
 
 ReactDOM.render(
 	<BrowserRouter>
 		<React.StrictMode>
-			<App />
+			<ThemeProvider theme={theme}>
+				<App />
+			</ThemeProvider>
 		</React.StrictMode>
 	</BrowserRouter>,
 	document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
