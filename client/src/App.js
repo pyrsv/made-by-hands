b75da60a299d9ef554575b122b8dae1321b39b0c
Sasha Kumeiko <sashaqmeyko@gmail.com>
2020-04-16 15:25:54 +0300

    WIP:multicarousel

--- client/src/App.js
+++ client/src/App.js
@@ -6,11 +6,13 @@ import Filter from './Pages/Filter';
 import Profile from './Pages/Profile';
 import Header from './Components/Header';
 import Button from './Components/UI/Button';
+import Multicarousel from './Components/Carousels/Multicarousel';
 
 function App() {
 	return (
 		<div>
 			<Header />
+			<Multicarousel />
 			<Button text="Отправить" color="light" icon="envelope" />
 			<Button text="Подробнее" color="dark" />
 			<Button text="Отправить" color="light" type="wide" />
