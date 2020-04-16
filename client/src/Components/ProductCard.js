48f0a16c8bb5b6bd889d4f52a3f5e24babbfa0e2
Nikita Pyresev <wegofender@gmail.com>
2020-04-16 12:03:31 +0300

    add ProductCart component

---
+++ client/src/Components/ProductCard.js
@@ -0,0 +1,36 @@
+import React from 'react';
+// import styled from 'styled-components';
+// import PropTypes from 'prop-types';
+
+// name,
+// img,
+// oldPrice,
+// price,
+// isInCart,
+// isFavorite,
+// onAddToCart,
+// onAddToFavorites,
+// type,
+
+const ProductCard = () => {
+	return <></>;
+};
+
+ProductCard.propTypes = {
+	// name: PropTypes.string.isRequired,
+	// img: PropTypes.string.isRequired,
+	// oldPrice: PropTypes.string,
+	// price: PropTypes.string.isRequired,
+	// isInCart: PropTypes.bool.isRequired,
+	// isFavorite: PropTypes.bool.isRequired,
+	// onAddToCart: PropTypes.func.isRequired,
+	// onAddToFavorites: PropTypes.func.isRequired,
+	// type: PropTypes.oneOf(['light', 'olive']),
+};
+
+ProductCard.defaultProps = {
+	// type: 'light',
+	// oldPrice: null,
+};
+
+export default ProductCard;
