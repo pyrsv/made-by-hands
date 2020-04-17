48b7b5e0daf0945f2d8922b7007d45d79975e0fe
Nikita Pyresev <wegofender@gmail.com>
2020-04-17 11:04:40 +0300

    finalize ProductCart with FavoriteHeart

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,57 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
 import ListOfItems from './Pages/ListOfItems';
 import Cart from './Pages/Cart';
 import Filter from './Pages/Filter';
 import Profile from './Pages/Profile';
 import Header from './Components/Header';
-import Button from './Components/UI/Button';
-import Multicarousel from './Components/Carousels/Multicarousel';
-import { getItemsAction } from './store/actions/getItemsAction';
+import ProductCard from './Components/ProductCard';
+
+const mock = {
+	name: 'Karl Kani Signature corduroy tape waist bag in camel',
+	currentPrice: 25.0,
+	categories: 'waist bags',
+	imageUrls: [
+		'https://res.cloudinary.com/dnorz3pyf/image/upload/v1586854755/waist%20bags/13977708-1-camelblack_ncxpsa.jpg',
+		'https://res.cloudinary.com/dnorz3pyf/image/upload/v1586854755/waist%20bags/13977708-2_prydxs.jpg',
+		'https://res.cloudinary.com/dnorz3pyf/image/upload/v1586854755/waist%20bags/13977708-3_yx1emi.jpg',
+		'https://res.cloudinary.com/dnorz3pyf/image/upload/e_improve,w_300,h_600,c_thumb,g_auto/v1586854755/waist%20bags/13977708-3_yx1emi.jpg',
+		'https://res.cloudinary.com/dnorz3pyf/image/upload/v1586854755/waist%20bags/13977708-4_gnqr8z.jpg',
+	],
+	quantity: 50,
+	color: 'yellow',
+	productUrl: '/waist-bags',
+	brand: 'Karl Kani',
+	parametrs: [
+		'Adjustable body strap',
+		'Secure clasp fastening',
+		'External pocket',
+		'Zip closure',
+	],
+	description: [
+		'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+		'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur temporibus ea amet numquam nihil in officiis explicabo pariatur totam nam, veritatis, ratione veniam maiores deserunt vitae voluptas nesciunt sed inventore. ',
+	],
+};
+
+const {
+	name,
+	currentPrice,
+	imageUrls: [image],
+} = mock;
 
 function App() {
-	const dispatch = useDispatch();
-
-	useEffect(() => {
-		dispatch(getItemsAction());
-	}, [dispatch]);
 	return (
 		<div>
 			<Header />
-			<Multicarousel />
-			<Button text="Отправить" color="light" icon="envelope" />
-			<Button text="Подробнее" color="dark" />
-			<Button text="Отправить" color="light" type="wide" />
+			<div style={{ width: '300px' }}>
+				<ProductCard
+					name={name}
+					price={currentPrice}
+					img={image}
+					type="olive"
+				/>
+			</div>
 
 			<Switch>
 				<Route exact path="/">
