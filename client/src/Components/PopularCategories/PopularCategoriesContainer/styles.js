c36b0a9a2b5683c55f1fbbd349a2f109faaba93a
Stanislav <staslyu1997@gmail.com>
2020-04-22 14:39:27 +0300

    Popular Categories

---
+++ client/src/Components/PopularCategories/PopularCategoriesContainer/styles.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const StyledPopularCategoriesContainer = styled.div`
+	text-align: center;
+`;
--- client/src/Pages/IndexPage.js
+++ client/src/Pages/IndexPage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../Components/Header/Header';
 import ProductsCarousel from '../Components/Carousels/ProductsCarousel/ProductsCarousel';
+import PopularCategories from '../Components/PopularCategories/PopularCategories';
 
 const IndexPage = () => {
 	return (
 		<>
 			<Header />
 			<ProductsCarousel />
+			<PopularCategories />
 		</>
 	);
 };
