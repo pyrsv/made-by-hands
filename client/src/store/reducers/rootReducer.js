d9e8fc21797b88e8f48bf9ef0e0f2a1ed5fd5d5d
Ksixey <52416003+Ksixey@users.noreply.github.com>
2020-04-24 19:48:29 +0300

    minHeight && delete files with Redux and change them on useEffect

--- client/src/store/reducers/rootReducer.js
+++ client/src/store/reducers/rootReducer.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
 import { itemsReducer } from './itemsReducer';
 import { authReducer } from './authReducer';
-import { infoCarouselReducer } from './infoCarouselReducer';
 
 const rootReducer = combineReducers({
 	itemsReducer,
 	auth: authReducer,
-	infoCarousel: infoCarouselReducer,
 });
 
 export default rootReducer;
