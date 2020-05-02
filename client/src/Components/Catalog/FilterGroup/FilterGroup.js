62f5f3bb7296b601ec50ad728cc24b44b4ae5426
Nikita Pyresev <wegofender@gmail.com>
2020-05-02 10:32:13 +0300

    structured FilterGroup component

---
+++ client/src/Components/Catalog/FilterGroup/FilterGroup.js
@@ -0,0 +1,22 @@
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// import Checkbox from '../../UI/Checkbox/Checkbox';
+// import { FiltersWrapper, FiltersName } from './styles';
+
+// const FilterGroup = ({ name, fields, values, valuesKey, onChange }) => {
+// 	return (
+// 		<FiltersWrapper>
+// 			<FiltersName>{name}</FiltersName>
+// 		</FiltersWrapper>
+// 	);
+// };
+
+// FilterGroup.propTypes = {
+// 	name: PropTypes.string.isRequired,
+// 	fields: PropTypes.arrayOf(PropTypes.object).isRequired,
+// 	values: PropTypes.objectOf(PropTypes.bool).isRequired,
+// 	valuesKey: PropTypes.string.isRequired,
+// 	onChange: PropTypes.func.isRequired,
+// };
+
+// export default FilterGroup;
---
+++ client/src/Components/Catalog/FilterGroup/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const FiltersWrapper = styled.div`
+	padding: 15px 0;
+`;
+
+export const FiltersName = styled.h4`
+	font-size: 28px;
+	margin-bottom: 15px;
+	@media (max-width: 992px) {
+		font-size: 22px;
+	}
+`;
