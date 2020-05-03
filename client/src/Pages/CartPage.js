949ef35893b0c0711287ad9d841994fbe22ab418
Sasha Kumeiko <sashaqmeyko@gmail.com>
2020-05-03 20:58:05 +0300

    add footer to cart

--- client/src/Pages/CartPage.js
+++ client/src/Pages/CartPage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Cart } from '../Components/Cart/Cart';
 import ProductsCarousel from '../Components/Carousels/ProductsCarousel/ProductsCarousel';
+import Footer from '../Components/Footer/Footer';
 
 const CartPage = () => {
 	return (
 		<div>
 			<Cart />
 			<ProductsCarousel />
+			<Footer />
 		</div>
 	);
 };
