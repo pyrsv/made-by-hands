17e6a7d86d7209166f0d41c2e70eacc86171f436
Nikita Pyresev <wegofender@gmail.com>
2020-05-04 10:04:15 +0300

    add filterGroup component

--- client/src/Components/Catalog/FilterGroup/FilterGroup.js
+++ client/src/Components/Catalog/FilterGroup/FilterGroup.js
@@ -1,22 +1,59 @@
-// import React from 'react';
-// import PropTypes from 'prop-types';
-// import Checkbox from '../../UI/Checkbox/Checkbox';
-// import { FiltersWrapper, FiltersName } from './styles';
+import React from 'react';
+import PropTypes from 'prop-types';
+import Checkbox from '../../UI/Checkbox/Checkbox';
+import { FiltersWrapper, FiltersName } from './styles';
 
-// const FilterGroup = ({ name, fields, values, valuesKey, onChange }) => {
-// 	return (
-// 		<FiltersWrapper>
-// 			<FiltersName>{name}</FiltersName>
-// 		</FiltersWrapper>
-// 	);
-// };
+const FilterGroup = ({
+	name,
+	fields,
+	values,
+	fieldsKey,
+	setValue,
+	checkboxType,
+	children,
+}) => {
+	return (
+		<FiltersWrapper>
+			<FiltersName>{name}</FiltersName>
+			{Object.entries(fields || {}).map(([key, val], index) => {
+				return (
+					<Checkbox
+						key={values[index]._id}
+						id={values[index]._id}
+						checked={val}
+						name={`.${key}`}
+						label={values[index].name}
+						onChange={() => setValue(`${fieldsKey}.${key}`, !val)}
+						type={checkboxType}
+						cssValue={values[index].cssValue}
+					/>
+				);
+			})}
+			{children}
+		</FiltersWrapper>
+	);
+};
 
-// FilterGroup.propTypes = {
-// 	name: PropTypes.string.isRequired,
-// 	fields: PropTypes.arrayOf(PropTypes.object).isRequired,
-// 	values: PropTypes.objectOf(PropTypes.bool).isRequired,
-// 	valuesKey: PropTypes.string.isRequired,
-// 	onChange: PropTypes.func.isRequired,
-// };
+FilterGroup.propTypes = {
+	name: PropTypes.string.isRequired,
+	fields: PropTypes.arrayOf(PropTypes.object),
+	values: PropTypes.objectOf(PropTypes.bool),
+	fieldsKey: PropTypes.string,
+	setValue: PropTypes.func,
+	checkboxType: PropTypes.string,
+	children: PropTypes.oneOfType([
+		PropTypes.arrayOf(PropTypes.node),
+		PropTypes.node,
+	]),
+};
 
-// export default FilterGroup;
+FilterGroup.defaultProps = {
+	fields: {},
+	values: [],
+	fieldsKey: '',
+	setValue: () => {},
+	checkboxType: '',
+	children: <></>,
+};
+
+export default FilterGroup;
