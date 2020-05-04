6e559946e0825e54512ddffafd4a9fd1cebb70d1
Ksixey <52416003+Ksixey@users.noreply.github.com>
2020-05-04 16:09:00 +0300

    page product

--- client/src/Pages/ProductPage.js
+++ client/src/Pages/ProductPage.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ProductDescription from '../Components/ProductDescription/ProductDescription';
 import PropTypes from 'prop-types';
+import ProductsCarousel from '../Components/Carousels/ProductsCarousel/ProductsCarousel';
+import Footer from '../Components/Footer/Footer';
 
 const ProductPage = ({ cartNoParam }) => {
 	return (
 		<>
 			<ProductDescription cartNoParam={cartNoParam} />
+			<ProductsCarousel />
+			<Footer />
 		</>
 	);
 };
