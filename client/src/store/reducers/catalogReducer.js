9e1071ca4e6c327c9e41f3cf2871e0222ca0dbb0
Nikita Pyresev <wegofender@gmail.com>
2020-05-06 09:13:27 +0300

    delete filters actions from catalog

--- client/src/store/reducers/catalogReducer.js
+++ client/src/store/reducers/catalogReducer.js
@@ -1,10 +1,7 @@
 import {
-	GET_CATEGORIES,
-	GET_COLORS,
 	GET_FILTERED_PRODUCTS_ERROR,
 	GET_FILTERED_PRODUCTS_INIT,
 	GET_FILTERED_PRODUCTS_SUCCESS,
-	GET_BRANDS,
 	UPDATE_CONFIG,
 	LOAD_MORE_PRODUCTS,
 	SET_PRODUCT_TO_CART,
@@ -12,9 +9,6 @@ import {
 } from '../types/catalogTypes';
 
 const initialState = {
-	categories: [],
-	colors: [],
-	brands: [],
 	isProductsFetching: false,
 	isProductsError: false,
 	currentProducts: [],
@@ -29,21 +23,6 @@ const initialState = {
 
 export const catalogReducer = (state = initialState, { type, payload }) => {
 	switch (type) {
-		case GET_CATEGORIES:
-			return {
-				...state,
-				categories: payload,
-			};
-		case GET_COLORS:
-			return {
-				...state,
-				colors: payload,
-			};
-		case GET_BRANDS:
-			return {
-				...state,
-				brands: payload,
-			};
 		case GET_FILTERED_PRODUCTS_INIT:
 			return {
 				...state,
