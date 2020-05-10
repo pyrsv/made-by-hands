20be14aca3e7411772fdfe707d8221fda36703ef
Nikita Pyresev <wegofender@gmail.com>
2020-05-10 12:46:35 +0300

    move window resize handler back to header

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import React from 'react';
 import { Route, Switch, useLocation } from 'react-router-dom';
 import CartPage from './Pages/CartPage';
 import CatalogPage from './Pages/CatalogPage';
@@ -8,51 +7,12 @@ import IndexPage from './Pages/IndexPage';
 import Header from './Components/Header/Header';
 import AppContainer from './Components/AppContainer/AppContainer';
 import ProtectedRoute from './Components/PrivateRoute/PrivateRoute';
-import { getUser } from './store/actions/authActions';
-
+import AuthModal from './Components/AuthModal/AuthModal';
 import Wishlist from './Components/Wishlist/Wishlist';
 
-import {
-	setHeaderMobile,
-	setMobile,
-	setTablet,
-} from './store/actions/UIActions';
-
 const App = () => {
-	const dispatch = useDispatch();
 	const location = useLocation();
 	const background = location.state && location.state.background;
-
-	const handleWindowResize = () => {
-		if (window.innerWidth <= 992) {
-			dispatch(setHeaderMobile(true));
-		} else {
-			dispatch(setHeaderMobile(false));
-		}
-
-		if (window.innerWidth <= 768) {
-			dispatch(setTablet(true));
-		} else {
-			dispatch(setTablet(false));
-		}
-
-		if (window.innerWidth <= 576) {
-			dispatch(setMobile(true));
-		} else {
-			dispatch(setMobile(false));
-		}
-	};
-
-	useEffect(() => {
-		handleWindowResize();
-
-		dispatch(getUser());
-
-		window.addEventListener('resize', handleWindowResize);
-		return () => {
-			window.removeEventListener('resize', handleWindowResize);
-		};
-	}, []);
 
 	return (
 		<AppContainer>
@@ -75,6 +35,12 @@ const App = () => {
 					<Wishlist />
 				</Route>
 			</Switch>
+
+			{background && (
+				<Route path="/login">
+					<AuthModal />
+				</Route>
+			)}
 		</AppContainer>
 	);
 };
