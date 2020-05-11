1af918c332d3a7493754001419a554ab42eb9bd2
Nikita Pyresev <wegofender@gmail.com>
2020-05-11 16:38:36 +0300

    styled ContactInfo component

--- client/src/Components/Profile/ContactInfo/styles.js
+++ client/src/Components/Profile/ContactInfo/styles.js
@@ -1,9 +1,13 @@
 import styled from 'styled-components';
 
-export const Description = styled.p``;
+export const Description = styled.p`
+	font-weight: 600;
+	font-size: 14px;
+	margin-bottom: 30px;
+`;
 
 export const InfoWrapper = styled.div``;
 
-export const FormColumn = styled.div``;
-
-export const Form = styled.form``;
+export const Form = styled.form`
+	max-width: 280px;
+`;
