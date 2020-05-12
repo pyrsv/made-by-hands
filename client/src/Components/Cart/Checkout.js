aefd571219b8ec570a627e7869319aca1cbe5112
Sasha Kumeiko <sashaqmeyko@gmail.com>
2020-05-12 15:10:22 +0300

    add conditional inputs

--- client/src/Components/Cart/Checkout.js
+++ client/src/Components/Cart/Checkout.js
@@ -1,5 +1,4 @@
-import React from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
+import React, { useState } from 'react';
 import { withFormik, Form } from 'formik';
 import * as yup from 'yup';
 import { string, number } from 'yup';
@@ -15,6 +14,18 @@ import {
 import PropTypes from 'prop-types';
 
 function FormData({ errors, touched, isValid, submitForm }) {
+	const [isPostalPoints, togglePP] = useState({ showed: false });
+	const [isAddress, toggleAddress] = useState({ showed: false });
+
+	const showPostalPoints = () => {
+		togglePP({ showed: true });
+		toggleAddress({ showed: false });
+	};
+	const showAddress = () => {
+		togglePP({ showed: false });
+		toggleAddress({ showed: true });
+	};
+
 	return (
 		<Form>
 			<FlexContainer>
@@ -43,12 +54,56 @@ function FormData({ errors, touched, isValid, submitForm }) {
 				</StyledFormColumn>
 				<StyledFormColumn>
 					<StyledLabel>
-						Choose postal delivery point
-						<StyledField as="select" name="delivery">
-							<option value="point1"> point 1</option>
-							<option value="point2"> point 2</option>
-						</StyledField>
+						<input
+							type="radio"
+							text="Delivery to postal point"
+							name="delivery"
+							onClick={() => {
+								showPostalPoints();
+							}}
+						/>
+						Delivery to postal point
 					</StyledLabel>
+					<StyledLabel>
+						<input
+							type="radio"
+							text="Delivery to address"
+							name="delivery"
+							onClick={() => showAddress()}
+						/>
+						Delivery to address
+					</StyledLabel>
+					{isPostalPoints.showed && (
+						<StyledLabel>
+							Choose postal delivery point
+							<StyledField as="select" name="delivery">
+								<option value="point1"> point 1</option>
+								<option value="point2"> point 2</option>
+							</StyledField>
+						</StyledLabel>
+					)}
+					{isAddress.showed && (
+						<>
+							<StyledLabel>
+								{' '}
+								Enter your city
+								<StyledField className="field" name="city" />
+							</StyledLabel>
+							<StyledLabel>
+								{' '}
+								Enter your street
+								<StyledField className="field" name="street" />
+							</StyledLabel>
+							<StyledLabel>
+								{' '}
+								Enter your house
+								<StyledField className="field" name="house" />
+							</StyledLabel>
+						</>
+					)}
+					{errors.city && touched.city ? <div>{errors.city}</div> : null}
+					{errors.street && touched.street ? <div>{errors.street}</div> : null}
+					{errors.house && touched.house ? <div>{errors.house}</div> : null}
 				</StyledFormColumn>
 			</FlexContainer>
 			<ButtonWrapper>
