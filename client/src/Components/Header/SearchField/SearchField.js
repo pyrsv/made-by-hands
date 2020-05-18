3458ba4bddd5235615edc34bfd88cfb019bd2e6e
Vadim Bevziuk <vadw1984@gmail.com>
2020-05-18 18:23:20 +0300

    footer with finder

--- client/src/Components/Header/SearchField/SearchField.js
+++ client/src/Components/Header/SearchField/SearchField.js
@@ -2,21 +2,68 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import InputField from '../../UI/InputFiels/InputField';
 import { SearchButton, SearchForm } from './styles';
+import { Formik } from 'formik';
+// import { DebounceInput } from 'react-debounce-input';
+import { getSearchProduct } from './getSearchProduct';
+// import { NavLink } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
+// const SearchField = () => {
+// 	return (
+// 		<SearchForm>
+// 			<InputField
+// 				type="text"
+// 				name="header-search"
+// 				onChange={() => {}}
+// 				placeholder="Search"
+// 				target="search"
+// 				value=""
+// 			/>
+// 			<SearchButton>
+// 				<FontAwesomeIcon icon={['fas', 'search']} />
+// 			</SearchButton>
+// 		</SearchForm>
+// 	);
+// };
+
+// export default SearchField;
 
 const SearchField = () => {
+	const dispatch = useDispatch();
+
 	return (
 		<SearchForm>
-			<InputField
-				type="text"
-				name="header-search"
-				onChange={() => {}}
-				placeholder="Search"
-				target="search"
-				value=""
-			/>
-			<SearchButton>
-				<FontAwesomeIcon icon={['fas', 'search']} />
-			</SearchButton>
+			<Formik
+				initialValues={{ headerSearch: '' }}
+				onSubmit={values => {
+					dispatch(getSearchProduct(values.headerSearch));
+					// console.log(values.headerSearch);
+				}}
+			>
+				{({ handleSubmit, handleChange }) => (
+					<SearchForm onSubmit={handleSubmit}>
+						<InputField
+							type="text"
+							name="headerSearch"
+							placeholder="Search"
+							onChange={handleChange}
+							required
+						>
+							{/* <DebounceInput
+								minLength={1}
+								debounceTimeout={100}
+								onChange={event => console.log(event.target.value)}
+							/> */}
+						</InputField>
+
+						<SearchButton>
+							{/* <NavLink to="/catalog"> */}
+							<FontAwesomeIcon icon={['fas', 'search']} />
+							{/* </NavLink> */}
+						</SearchButton>
+					</SearchForm>
+				)}
+			</Formik>
 		</SearchForm>
 	);
 };
---
+++ client/src/Components/Header/SearchField/getSearchProduct.js
@@ -0,0 +1,69 @@
+import axios from 'axios';
+import { checkProductsForCartAndFavorites } from '../../../utils/API';
+import { getFilteredProductsSuccess } from '../../../store/actions/catalogActions';
+// import { useDispatch } from 'react-redux';
+
+export const getSearchProduct = value => dispatch => {
+	const searchPhrases = {
+		query: value,
+	};
+
+	axios
+		.post('/products/search', searchPhrases)
+		.then(res => {
+			const clients = [];
+			// console.log(res.data);
+			const result = res.data;
+			result.map(el => {
+				if (el.name.includes(value)) {
+					clients.push(el);
+					// console.log(el);
+				}
+				return clients;
+			});
+
+			checkProductsForCartAndFavorites(clients).then(
+				productsWithCartAndFavorites => {
+					// console.log(productsWithCartAndFavorites);
+					dispatch(
+						getFilteredProductsSuccess(
+							productsWithCartAndFavorites,
+							clients.productsQuantity
+						)
+					);
+				}
+			);
+			// console.log(clients);
+		})
+		.catch(err => err, 'error request');
+	// ------------------------------------------------------------------------------
+	// axios
+	// 	.get("/products")
+	// 	// .get("/products/search", {'query':value})
+	// 	.then(res => {
+
+	// 		const clients = [];
+	// 		// console.log(res);
+	// 		res.data.map(el => {
+	// 			if (el.name.includes(value)) {
+	// 				clients.push(el);
+	// 				// console.log(el);
+	// 			}
+	// 		});
+
+	// checkProductsForCartAndFavorites(clients).then(
+	// 	productsWithCartAndFavorites => {
+	// 		console.log(productsWithCartAndFavorites);
+	// 		dispatch(
+	// 			getFilteredProductsSuccess(
+	// 				productsWithCartAndFavorites,
+	// 				clients.productsQuantity
+	// 			)
+	// 		);
+	// 	}
+	// );
+	// 		console.log(clients);
+	// 	})
+
+	// 	.catch(err => err, 'error request');
+};
