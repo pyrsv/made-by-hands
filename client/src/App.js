02e02255d89a07bb8c7aa1e579a5c82272a4b808
Nikita Pyresev <wegofender@gmail.com>
2020-05-21 19:31:08 +0300

    move routes to separate file

--- client/src/App.js
+++ client/src/App.js
@@ -1,54 +1,15 @@
 import React from 'react';
-import { Route, Switch, useLocation } from 'react-router-dom';
-import CartPage from './Pages/CartPage';
-import CatalogPage from './Pages/CatalogPage';
-import ProfilePage from './Pages/ProfilePage';
-import IndexPage from './Pages/IndexPage';
-import ProductPage from './Pages/ProductPage';
-import NoMatchPage from './Pages/NoMatchPage';
 import Header from './Components/Header/Header';
 import AppContainer from './Components/AppContainer/AppContainer';
-import ProtectedRoute from './HOC/ProtectedRoute/ProtectedRoute';
-import AuthModal from './Components/AuthModal/AuthModal';
+import Routes from './Routes/Routes';
+import Footer from './Components/Footer/Footer';
 
 const App = () => {
-	const location = useLocation();
-	const background = location.state && location.state.background;
-
 	return (
 		<AppContainer>
 			<Header />
-			<Switch location={background || location}>
-				<Route exact path="/">
-					<IndexPage />
-				</Route>
-				<Route path="/cart">
-					<CartPage />
-				</Route>
-				<Route path="/catalog">
-					<CatalogPage />
-				</Route>
-				<ProtectedRoute path="/profile">
-					<ProfilePage />
-				</ProtectedRoute>
-				<Route
-					path="/products/:itemNo"
-					render={({ match }) => {
-						return (
-							match && <ProductPage productNoParam={match.params.itemNo} />
-						);
-					}}
-				/>
-				<Route path="*">
-					<NoMatchPage />
-				</Route>
-			</Switch>
-
-			{background && (
-				<Route path="/login">
-					<AuthModal />
-				</Route>
-			)}
+			<Routes />
+			<Footer />
 		</AppContainer>
 	);
 };
