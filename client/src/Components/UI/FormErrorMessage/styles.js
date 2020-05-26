78b9496fa01ab8f12bd96506163cfcecb2261e4c
Nikita Pyresev <wegofender@gmail.com>
2020-05-26 14:05:00 +0300

    add FormErrorMessage component

---
+++ client/src/Components/UI/FormErrorMessage/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Message = styled.span`
+	display: inline-block;
+	font-size: 16px;
+	padding: 7px 0;
+	color: ${props => props.theme.accentRed};
+`;
