523664da5b382227f12a9362d48240bd32bbc530
Nikita Pyresev <wegofender@gmail.com>
2020-05-27 10:07:34 +0300

    create SearchList component

---
+++ client/src/Components/SearchList/SearchList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// import { useSelector, useDispatch } from 'react-redux';
+
+const SearchList = () => {
+	// const dispatch = useDispatch();
+
+	return <></>;
+};
+
+export default SearchList;
