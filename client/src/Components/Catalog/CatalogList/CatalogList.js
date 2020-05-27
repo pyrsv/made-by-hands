2d8bde57e5466b439fbe8f8bb88404cc94fb4d40
Nikita Pyresev <wegofender@gmail.com>
2020-05-27 10:40:17 +0300

    create CatalogList conponent using reusable ProductsList

---
+++ client/src/Components/Catalog/CatalogList/CatalogList.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useMemo } from 'react';
+import querystring from 'query-string';
+import { useSelector, useDispatch } from 'react-redux';
+import { useLocation } from 'react-router-dom';
+import ProductsList from '../../ProductsList/ProductsList';
+import {
+	getFilteredProducts,
+	updateConfig,
+} from '../../../store/actions/catalogActions';
+
+const CatalogList = () => {
+	const dispatch = useDispatch();
+	const location = useLocation();
+	const products = useSelector(state => state.catalog.currentProducts);
+	const productsQuantity = useSelector(state => state.catalog.productsQuantity);
+	const perPage = useSelector(state => state.catalog.perPage);
+	const startPage = useSelector(state => state.catalog.startPage);
+	const isProductsFetching = useSelector(
+		state => state.catalog.isProductsFetching
+	);
+
+	const currentParams = useMemo(
+		() => querystring.parse(location.search.slice(1)),
+		[location.search]
+	);
+
+	useEffect(() => {
+		dispatch(getFilteredProducts({ perPage, ...currentParams, startPage: 1 }));
+		return () => dispatch(updateConfig({ perPage: 12, startPage: 1 }));
+	}, [location, dispatch, perPage, currentParams]);
+
+	return (
+		<ProductsList
+			products={products}
+			productsQuantity={productsQuantity}
+			queryParams={currentParams}
+			startPage={startPage}
+			perPage={perPage}
+			isProductsFetching={isProductsFetching}
+		/>
+	);
+};
+
+export default CatalogList;
