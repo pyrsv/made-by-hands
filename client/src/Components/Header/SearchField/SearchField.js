5890d7bc382da93857151cc4f7b63d732be9426e
Nikita Pyresev <wegofender@gmail.com>
2020-05-27 11:57:01 +0300

    wrapped SearchField in Formik

--- client/src/Components/Header/SearchField/SearchField.js
+++ client/src/Components/Header/SearchField/SearchField.js
@@ -1,23 +1,28 @@
 import React from 'react';
+import { Formik } from 'formik';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import InputField from '../../UI/InputFiels/InputField';
 import { SearchButton, SearchForm } from './styles';
 
 const SearchField = () => {
 	return (
-		<SearchForm>
-			<InputField
-				type="text"
-				name="header-search"
-				onChange={() => {}}
-				placeholder="Search"
-				target="search"
-				value=""
-			/>
-			<SearchButton>
-				<FontAwesomeIcon icon={['fas', 'search']} />
-			</SearchButton>
-		</SearchForm>
+		<Formik initialValues={{ search: '' }} onSubmit={() => {}}>
+			{({ values, handleSubmit, handleChange }) => (
+				<SearchForm onSubmit={handleSubmit}>
+					<InputField
+						type="text"
+						name="search"
+						onChange={handleChange}
+						placeholder="Search"
+						target="search"
+						value={values.search}
+					/>
+					<SearchButton>
+						<FontAwesomeIcon icon={['fas', 'search']} />
+					</SearchButton>
+				</SearchForm>
+			)}
+		</Formik>
 	);
 };
 
