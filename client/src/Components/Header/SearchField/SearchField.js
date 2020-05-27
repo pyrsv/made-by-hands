4575440fd5d8d73fad05ea00c9b71f919e349d38
Nikita Pyresev <wegofender@gmail.com>
2020-05-27 22:20:30 +0300

    searchFileld changes querystring on form submit

--- client/src/Components/Header/SearchField/SearchField.js
+++ client/src/Components/Header/SearchField/SearchField.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import { Formik } from 'formik';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import InputField from '../../UI/InputFiels/InputField';
 import { SearchButton, SearchForm } from './styles';
 
 const SearchField = () => {
+	const history = useHistory();
+
+	const handleSearchSubmit = ({ search }) => {
+		history.push(`/search?query=${encodeURI(search.trim())}`);
+	};
+
 	return (
-		<Formik initialValues={{ search: '' }} onSubmit={() => {}}>
+		<Formik initialValues={{ search: '' }} onSubmit={handleSearchSubmit}>
 			{({ values, handleSubmit, handleChange }) => (
 				<SearchForm onSubmit={handleSubmit}>
 					<InputField
