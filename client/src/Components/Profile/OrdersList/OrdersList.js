37f8cd2faeb0b690c47f0af1d65d9cf9e37bcdb9
Nikita Pyresev <wegofender@gmail.com>
2020-06-02 20:39:40 +0300

    add orders list with adaptive

--- client/src/Components/Profile/OrdersList/OrdersList.js
+++ client/src/Components/Profile/OrdersList/OrdersList.js
@@ -1,12 +1,31 @@
 // import React, { useState, useEffect } from 'react';
-import React, { useEffect } from 'react';
-import {} from './styles';
+import React, { useEffect, useState } from 'react';
+import Order from './Order/Order';
+import { OrdersWrapper } from './styles';
 
 const OrdersList = () => {
-	// const [orders, setOrders] = useState([]);
+	const [orders, setOrders] = useState([]);
 	// const [isFetching, setFetching] = useState(false);
-	useEffect(() => {}, []);
-	return <></>;
+	useEffect(() => {
+		// setFetching(true);
+		fetch('./mock.json', {
+			headers: {
+				Accept: 'application/json',
+			},
+		})
+			.then(res => res.json())
+			.then(res => {
+				// setFetching(true);
+				setOrders(res);
+			});
+	}, []);
+	return (
+		<OrdersWrapper>
+			{orders.map(order => (
+				<Order key={order._id} {...order} />
+			))}
+		</OrdersWrapper>
+	);
 };
 
 export default OrdersList;
