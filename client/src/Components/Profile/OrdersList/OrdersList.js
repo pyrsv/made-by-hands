3641e0ed7d59faa3345e5e6809737ce7d2e47a13
Nikita Pyresev <wegofender@gmail.com>
2020-06-04 09:58:37 +0300

    remove unnesessaty comments

--- client/src/Components/Profile/OrdersList/OrdersList.js
+++ client/src/Components/Profile/OrdersList/OrdersList.js
@@ -29,30 +29,3 @@ const OrdersList = () => {
 };
 
 export default OrdersList;
-
-// order data model
-// const order = {
-// 	customerId: customerId || null,
-// 	email: String,
-// 	mobile: String,
-// 	deliveryAddress:
-// 		{
-// 			country: 'Ukraine',
-// 			city: 'Kyiv', // only Kyiv may be there
-// 			address: 'Kreshchatic Street 56//A',
-// 		} || null,
-// 	postalDelivery:
-// 		{
-// 			city: String,
-// 			postalOffice: String,
-// 		} || null,
-// 	status: 'Pending', // (Обрабатывается)
-// 	letterSubject: String,
-// 	letterHTML: String,
-
-// 	// for non-authenticataed user
-// 	firstName: String,
-// 	lastName: String,
-// 	products: [], // Array of products,must have the same structure,
-// 	// as in cart, but the full data about product instead of only ObjectId.
-// };
