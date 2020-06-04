26f9c43bb5ce4c4a466014258c81bb94b0d94b68
Nikita Pyresev <wegofender@gmail.com>
2020-06-04 08:45:34 +0300

    fixed SearchList for cart and favorites

--- client/src/Components/Search/SearchList/SearchList.js
+++ client/src/Components/Search/SearchList/SearchList.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import querystring from 'query-string';
 import { useSelector, useDispatch } from 'react-redux';
-import { searchProducts } from '../../../store/actions/searchActions';
+import { searchProducts } from '../../../store/actions/catalogActions';
 import ProductsList from '../../ProductsList/ProductsList';
 import LayoutContainer from '../../LayoutContainer/LayoutContainer';
 
@@ -10,12 +10,13 @@ const SearchList = () => {
 	const dispatch = useDispatch();
 	const location = useLocation();
 
-	const products = useSelector(state => state.search.products);
-	const isFetching = useSelector(state => state.search.isFetching);
+	const products = useSelector(state => state.catalog.currentProducts);
+	const isFetching = useSelector(state => state.catalog.isProductsFetching);
 
 	const searchQuery = querystring.parse(location.search, {
 		arrayFormat: 'comma',
 	}).query;
+
 	useEffect(() => {
 		dispatch(searchProducts(searchQuery));
 	}, [searchQuery, dispatch]);
