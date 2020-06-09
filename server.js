b1320dca8375670c333d7c8dde061cbe852279d1
Sasha Kumeiko <sashaqmeyko@gmail.com>
2020-06-09 23:11:05 +0300

    delete line from server.js

--- server.js
+++ server.js
@@ -30,7 +30,6 @@ const app = express();
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use('/', routes)
 
 // DB Config
 const db = require("./config/keys").mongoURI;
