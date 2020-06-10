9f3d18e6b1df92d20953a3d35631868b63ba7507
Sasha Kumeiko <sashaqmeyko@gmail.com>
2020-06-10 12:11:57 +0300

    change port in server.js

--- server.js
+++ server.js
@@ -83,6 +83,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
