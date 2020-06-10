e015e71ab317406ad1b3b1b410823825f0772ca7
Sasha Kumeiko <sashaqmeyko@gmail.com>
2020-06-10 17:56:01 +0300

    change favicon ico path

--- server.js
+++ server.js
@@ -75,8 +75,6 @@ if (process.env.NODE_ENV === "production") {
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    res.sendfile('./public/main.html');
-
   });
 }
 
