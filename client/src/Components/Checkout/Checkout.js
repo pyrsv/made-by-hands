01265f1b6153ed26ee8bccee3d1f388406627c9d
Nikita Pyresev <wegofender@gmail.com>
2020-06-20 17:39:29 +0300

    add checkout page

---
+++ client/src/Components/Checkout/Checkout.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+import Title from '../UI/Title/title';
+import LayoutContainer from '../LayoutContainer/LayoutContainer';
+import {} from './styles';
+
+const Checkout = () => {
+	return (
+		<LayoutContainer>
+			<Title text="checkout" color="dark" />
+		</LayoutContainer>
+	);
+};
+
+export default Checkout;
