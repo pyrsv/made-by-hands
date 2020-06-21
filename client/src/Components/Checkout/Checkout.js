be94e24ae60adba8c26ddc2bb888d42ca32ab560
Nikita Pyresev <wegofender@gmail.com>
2020-06-21 13:23:16 +0300

    add dynamic validation schema into Checkout form

--- client/src/Components/Checkout/Checkout.js
+++ client/src/Components/Checkout/Checkout.js
@@ -2,21 +2,13 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { useSelector } from 'react-redux';
 import { Formik, Field } from 'formik';
-// import * as yup from 'yup';
-import { string, number } from 'yup';
+import * as Yup from 'yup';
 import Title from '../UI/Title/title';
 import Button from '../UI/Button/Button';
 import LayoutContainer from '../LayoutContainer/LayoutContainer';
 import CheckoutSumbit from './CheckoutSubmit/CheckoutSubmit';
 import InputField from '../UI/InputFiels/InputField';
 import {} from './styles';
-
-const valid = {
-	firstName: string().required(),
-	lastName: string().required(),
-	phone: number().required().positive().integer(),
-	email: string().email(),
-};
 
 const Checkout = () => {
 	const [isAddressDelivery, setAddressDelivery] = useState(false);
@@ -28,6 +20,40 @@ const Checkout = () => {
 	if (user && !user.address) {
 		user.address = {};
 	}
+
+	const checkoutSchema = Yup.object().shape({
+		firstName: Yup.string()
+			.matches(/^[a-zA-Zа-яА-Я]+$/, {
+				message: 'Allowed characters is a-z, A-Z, а-я, А-Я.',
+			})
+			.min(2, 'First name must be between 2 and 25 characters')
+			.required('First name is required'),
+		lastName: Yup.string()
+			.matches(/^[a-zA-Zа-яА-Я]+$/, {
+				message: 'Allowed characters is a-z, A-Z, а-я, А-Я.',
+			})
+			.min(2, 'Last Name must be between 2 and 25 characters')
+			.required('Last name is required'),
+		email: Yup.string()
+			.email('Enter valid email')
+			.required('Email is requiered'),
+		telephone: Yup.string()
+			.matches(/^\+380\d{3}\d{2}\d{2}\d{2}$/, {
+				message: 'Enter phone number in format +380XXXXXXXXX',
+			})
+			.required('Phone is required'),
+		delivery: isAddressDelivery
+			? Yup.object().shape({
+					city: Yup.string().required('City is required'),
+					house: Yup.string().required('House number is required'),
+					app: Yup.string().required('Flat is required'),
+					street: Yup.string().required('Street is required'),
+			  })
+			: Yup.object().shape({
+					deliveryCity: Yup.string().required(),
+					deliveryPoint: Yup.string().required(),
+			  }),
+	});
 
 	const handleFormSubmit = values => {
 		let checkoutData;
@@ -46,15 +72,15 @@ const Checkout = () => {
 		checkoutData.email = values.email;
 		checkoutData.postalDelivery = !isAddressDelivery
 			? {
-					city: values.deliveryCity,
-					postalOffice: values.deliveryPoint,
+					city: values.delivery.deliveryCity,
+					postalOffice: values.delivery.deliveryPoint,
 			  }
 			: null;
 		checkoutData.deliveryAddress = isAddressDelivery
 			? JSON.stringify({
 					country: 'Ukraine',
-					city: values.city,
-					address: `${values.street} ${values.house}, fl.${values.app}`,
+					city: values.delivery.city,
+					address: `${values.delivery.street} ${values.delivery.house}, fl.${values.delivery.app}`,
 			  })
 			: null;
 		checkoutData.letterSubject = 'Thank you for order!';
@@ -70,24 +96,6 @@ const Checkout = () => {
 			.catch(err => err);
 	};
 
-	const showPostalPoints = errors => {
-		setAddressDelivery(false);
-		errors.city = '';
-		errors.street = '';
-		errors.house = '';
-		errors.app = '';
-		delete valid.city;
-		delete valid.street;
-		delete valid.house;
-	};
-	const showAddress = values => {
-		setAddressDelivery(true);
-		valid.city = string().required();
-		valid.street = string().required();
-		valid.house = string().required();
-		valid.app = number().required();
-		values.delivery = '';
-	};
 	return (
 		<LayoutContainer>
 			{isCheckoutSuccess ? (
@@ -102,34 +110,17 @@ const Checkout = () => {
 							lastName: user.lastName || '',
 							email: user.email || '',
 							telephone: user.telephone || '',
-							deliveryPoint: 'point1',
-							deliveryCity: 'Kyiv',
-							city: user?.address.city || '',
-							house: user?.address.houseNumber || '',
-							app: user?.address.flat || '',
-							street: user?.address.street || '',
+							delivery: {
+								deliveryPoint: '2',
+								deliveryCity: 'Kyiv',
+								city: user?.address.city || '',
+								house: user?.address.houseNumber || '',
+								app: user?.address.flat || '',
+								street: user?.address.street || '',
+							},
 						}}
-						// isInitialValid={false}
-						// validate={values => {
-						// 	const errors = {};
-						// 	if (valid.city) {
-						// 		if (!values.city || !isNaN(+values.street)) {
-						// 			errors.city = 'city is required as a string';
-						// 		}
-						// 		if (!values.street || !isNaN(+values.street)) {
-						// 			errors.street = 'Street is required as a string';
-						// 		}
-						// 		if (!values.house) {
-						// 			errors.house = 'house is required';
-						// 		}
-						// 		if (!values.app || isNaN(+values.app)) {
-						// 			errors.app = 'app is required as a number';
-						// 		}
-						// 	}
-						// 	return errors;
-						// }}
-						// validationSchema={yup.object().shape(valid)}
 						onSubmit={handleFormSubmit}
+						validationSchema={checkoutSchema}
 					>
 						{({
 							errors,
@@ -138,182 +129,180 @@ const Checkout = () => {
 							handleBlur,
 							handleChange,
 							handleSubmit,
-						}) => (
-							<form onSubmit={handleSubmit}>
-								<p>Fill the fields</p>
-								<div>
+						}) => {
+							return (
+								<form onSubmit={handleSubmit}>
+									<p>Fill the fields</p>
 									<div>
-										<InputField
-											name="firstName"
-											onChange={handleChange}
-											onBlur={handleBlur}
-											target="form"
-											type="text"
-											label="First Name"
-											value={values.firstName}
-											touched={touched.firstName}
-											error={errors.firstName}
-											helperText={errors.firstName}
-										/>
-										<InputField
-											name="lastName"
-											onChange={handleChange}
-											onBlur={handleBlur}
-											target="form"
-											type="text"
-											label="Last Name"
-											value={values.lastName}
-											touched={touched.lastName}
-											error={errors.lastName}
-											helperText={errors.lastName}
-										/>
-										<InputField
-											name="email"
-											onChange={handleChange}
-											onBlur={handleBlur}
-											target="form"
-											type="text"
-											label="Email"
-											value={values.email}
-											touched={touched.email}
-											error={errors.email}
-											helperText={errors.email}
-										/>
-										<InputField
-											name="telephone"
-											onChange={handleChange}
-											onBlur={handleBlur}
-											target="form"
-											type="text"
-											label="Phone number"
-											value={values.telephone}
-											touched={touched.telephone}
-											error={errors.telephone}
-											helperText={errors.telephone}
-										/>
-									</div>
+										<div>
+											<InputField
+												name="firstName"
+												onChange={handleChange}
+												onBlur={handleBlur}
+												target="form"
+												type="text"
+												label="First Name"
+												value={values.firstName}
+												touched={touched.firstName}
+												error={errors.firstName}
+												helperText={errors.firstName}
+											/>
+											<InputField
+												name="lastName"
+												onChange={handleChange}
+												onBlur={handleBlur}
+												target="form"
+												type="text"
+												label="Last Name"
+												value={values.lastName}
+												touched={touched.lastName}
+												error={errors.lastName}
+												helperText={errors.lastName}
+											/>
+											<InputField
+												name="email"
+												onChange={handleChange}
+												onBlur={handleBlur}
+												target="form"
+												type="text"
+												label="Email"
+												value={values.email}
+												touched={touched.email}
+												error={errors.email}
+												helperText={errors.email}
+											/>
+											<InputField
+												name="telephone"
+												onChange={handleChange}
+												onBlur={handleBlur}
+												target="form"
+												type="text"
+												label="Phone number"
+												value={values.telephone}
+												touched={touched.telephone}
+												error={errors.telephone}
+												helperText={errors.telephone}
+											/>
+										</div>
 
-									<div>
 										<div>
-											<input
-												checked={!isAddressDelivery}
-												value="postal"
-												type="radio"
-												text="Delivery to postal point"
-												name="delivery"
-												onChange={() => {
-													showPostalPoints(errors, values);
-												}}
-											/>
-											Delivery to the post
-										</div>
-										<div>
-											<input
-												checked={isAddressDelivery}
-												value="address"
-												type="radio"
-												text="Delivery to your address"
-												name="delivery"
-												onChange={() => showAddress(values)}
-											/>
-											Delivery to your address
-										</div>
-									</div>
-									<div>
-										{!isAddressDelivery ? (
-											// eslint-disable-next-line jsx-a11y/label-has-associated-control
-											<label>
-												Choose postal delivery point
-												<Field
-													as="select"
-													name="postalCity"
-													value={values.delivery}
-													onChange={handleChange}
-													onBlur={handleBlur}
-													type="select"
-												>
-													<option value="Kyiv"> Kyiv</option>
-													<option value="Lviv"> Lviv</option>
-													<option value="Odessa"> Odessa</option>
-												</Field>
-												<Field
-													as="select"
-													name="postalPoint"
-													value={values.delivery}
-													onChange={handleChange}
-													onBlur={handleBlur}
-													type="select"
-												>
-													<option value="point1"> Point 1</option>
-													<option value="point2"> Point 2</option>
-													<option value="point3"> Point 3</option>
-												</Field>
-											</label>
-										) : (
 											<div>
-												<InputField
-													name="city"
-													onChange={handleChange}
-													onBlur={handleBlur}
-													target="form"
-													type="text"
-													label="City"
-													value={values.city}
-													touched={touched.city}
-													error={errors.city}
-													helperText={errors.city}
+												<input
+													checked={!isAddressDelivery}
+													value="postal"
+													type="radio"
+													text="Delivery to postal point"
+													name="delivery"
+													onChange={() => {
+														setAddressDelivery(true);
+													}}
 												/>
-												<InputField
-													name="street"
-													onChange={handleChange}
-													onBlur={handleBlur}
-													target="form"
-													type="text"
-													label="Street"
-													value={values.street}
-													touched={touched.street}
-													error={errors.street}
-													helperText={errors.street}
-												/>
-												<InputField
-													name="house"
-													onChange={handleChange}
-													onBlur={handleBlur}
-													target="form"
-													type="text"
-													label="House number"
-													value={values.house}
-													touched={touched.house}
-													error={errors.house}
-													helperText={errors.house}
-												/>
-
-												<InputField
-													name="app"
-													onChange={handleChange}
-													onBlur={handleBlur}
-													target="form"
-													type="text"
-													label="Flat number"
-													value={values.app}
-													touched={touched.app}
-													error={errors.app}
-													helperText={errors.app}
-												/>
+												Delivery to the post
 											</div>
-										)}
+											<div>
+												<input
+													checked={isAddressDelivery}
+													value="address"
+													type="radio"
+													text="Delivery to your address"
+													name="delivery"
+													onChange={() => setAddressDelivery(false)}
+												/>
+												Delivery to your address
+											</div>
+										</div>
+										<div>
+											{!isAddressDelivery ? (
+												// eslint-disable-next-line jsx-a11y/label-has-associated-control
+												<label>
+													Choose postal delivery point
+													<Field
+														as="select"
+														name="delivery.postalCity"
+														value={values.delivery.deliveryCity}
+														onChange={handleChange}
+														onBlur={handleBlur}
+														type="select"
+													>
+														<option value="Kyiv"> Kyiv</option>
+														<option value="Lviv"> Lviv</option>
+														<option value="Odessa"> Odessa</option>
+													</Field>
+													<Field
+														as="select"
+														name="delivery.postalPoint"
+														value={values.delivery.postalPoint}
+														onChange={handleChange}
+														onBlur={handleBlur}
+														type="select"
+													>
+														<option value="1"> Point 1</option>
+														<option value="2"> Point 2</option>
+														<option value="3"> Point 3</option>
+													</Field>
+												</label>
+											) : (
+												<div>
+													<InputField
+														name="delivery.city"
+														onChange={handleChange}
+														onBlur={handleBlur}
+														target="form"
+														type="text"
+														label="City"
+														value={values.delivery.city}
+														touched={touched.delivery?.city}
+														error={errors.delivery?.city}
+													/>
+													<InputField
+														name="delivery.street"
+														onChange={handleChange}
+														onBlur={handleBlur}
+														target="form"
+														type="text"
+														label="Street"
+														value={values.delivery.street}
+														touched={touched.delivery?.street}
+														error={errors.delivery?.street}
+													/>
+													<InputField
+														name="delivery.house"
+														onChange={handleChange}
+														onBlur={handleBlur}
+														target="form"
+														type="text"
+														label="House number"
+														value={values.delivery.house}
+														touched={touched.delivery?.house}
+														error={errors.delivery?.house}
+													/>
+
+													<InputField
+														name="delivery.app"
+														onChange={handleChange}
+														onBlur={handleBlur}
+														target="form"
+														type="text"
+														label="Flat number"
+														value={values.delivery.app}
+														touched={touched.delivery?.app}
+														error={errors.delivery?.app}
+													/>
+												</div>
+											)}
+										</div>
 									</div>
-								</div>
-								<div>
-									<Button
-										onClick={() => {}}
-										type="submit"
-										text="checkout"
-										color="dark"
-									/>
-								</div>
-							</form>
-						)}
+									<div>
+										<Button
+											onClick={() => {}}
+											type="submit"
+											text="checkout"
+											color="dark"
+										/>
+									</div>
+								</form>
+							);
+						}}
 					</Formik>
 				</>
 			)}
