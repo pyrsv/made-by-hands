ac86ecd64dc2367fced1ba5b02bcfff7801805d2
Stanislav <staslyu1997@gmail.com>
2020-06-22 12:38:03 +0300

    Add link in Popular Categories

--- client/src/Components/PopularCategories/PopularCategoriesContainer/PopularCategoriesContainer.js
+++ client/src/Components/PopularCategories/PopularCategoriesContainer/PopularCategoriesContainer.js
@@ -3,13 +3,14 @@ import React from 'react';
 import Description from '../Description/Description';
 
 const PopularCategoriesContainer = () => {
-	const categories = useSelector(state => state.filters.categories.slice(0, 3));
+	const categories = useSelector(state => state.filters.categories.slice(1, 4));
 
 	return (
 		<>
 			{categories.map((category, index) => (
 				<Description
 					key={category.name}
+					url={category.name}
 					isReverse={index % 2 === 0}
 					name={category.name}
 					image={category.imgUrl}
